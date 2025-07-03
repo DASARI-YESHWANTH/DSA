a17a2cd0fa196b742612635a6e9b306cfe3c092a
DASARI-YESHWANTH <diddiyeshwanth09@gmail.com>
2025-07-03 12:29:33 +0530

    Time: 11 ms (27.49%) | Memory: 22.3 MB (82.67%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -2,7 +2,15 @@ class Solution(object):
     def rotate(self, nums, k):
         n=len(nums)
         k=k%n
-        nums[:]=nums[n-k:]+nums[0:n-k]
+        # nums[:]=nums[n-k:]+nums[0:n-k]
+        # return nums
+        def reverse(arr,l,r):
+            while l<r:
+                arr[l], arr[r] = arr[r], arr[l]
+                l += 1
+                r -= 1
+        reverse(nums,n-k,n-1)    
+        reverse(nums,0,n-k-1)
+        reverse(nums,0,n-1)
         return nums
-        
     
